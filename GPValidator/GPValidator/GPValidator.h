c6f6c0e220153117a564cb89b455e5f23b8431a5
gyana prakash gouda <gyanaprakashgouda471@gmail.com>
2016-04-07 00:04:29 +0530

    Validator test

---
+++ GPValidator/GPValidator/GPValidator.h
@@ -0,0 +1,13 @@
+//
+//  GPValidator.h
+//  GPValidator
+//
+//  Created by GYANA PRAKASH GOUDA on 06/04/16.
+//  Copyright © 2016 Thinkdifferent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GPValidator : NSObject
+
+@end
---
+++ GPValidator/GPValidator/GPValidator.m
@@ -0,0 +1,13 @@
+//
+//  GPValidator.m
+//  GPValidator
+//
+//  Created by GYANA PRAKASH GOUDA on 06/04/16.
+//  Copyright © 2016 Thinkdifferent. All rights reserved.
+//
+
+#import "GPValidator.h"
+
+@implementation GPValidator
+
+@end
